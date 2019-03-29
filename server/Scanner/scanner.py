d077dc12d27ebe6219fe00203530c8ff8628bde8
Mahe-Thomas <thomas.mahe@etu.unice.fr>
2019-03-29 19:52:22 +0100

    Fix angles controller moteur

--- server/Scanner/scanner.py
+++ server/Scanner/scanner.py
@@ -1,6 +1,6 @@
 from time import sleep
 import RPi.GPIO as GPIO
-from picamera import PiCamera
+#from picamera import PiCamera
 
 class StepperMotor :
 
@@ -18,15 +18,17 @@ class StepperMotor :
         GPIO.setup(self.DIR_PIN, GPIO.OUT)
         GPIO.setup(self.STEP_PIN, GPIO.OUT)
         GPIO.setup(self.ENABLE_PIN, GPIO.OUT)
+        GPIO.output(self.DIR_PIN, self.CW)
 
     def turn(self, degree):
+        GPIO.setmode(GPIO.BCM)
         if degree < 0 :
             GPIO.output(self.DIR_PIN, self.CW)
         else :
             GPIO.output(self.DIR_PIN, self.CCW)
 
         delay = 0.005 / 64
-        step_count = int((self.SPR / 360) * degree)
+        step_count = int((self.SPR / 360) * abs(degree))
         GPIO.output(self.ENABLE_PIN, GPIO.LOW)
         for x in range(step_count):
             GPIO.output(self.STEP_PIN, GPIO.HIGH)
@@ -34,8 +36,6 @@ class StepperMotor :
             GPIO.output(self.STEP_PIN, GPIO.LOW)
             sleep(delay)
         GPIO.output(self.ENABLE_PIN, GPIO.LOW)
-        GPIO.cleanup()
-
 
 
 
@@ -43,13 +43,14 @@ class Scanner :
 
     def __init__(self):
 
-
         self.bed_rotation = 0
         self.cam_rotation = 0
 
-        self.camera = PiCamera()
+        #self.camera = PiCamera()
 
-        self.bed_motor = StepperMotor(20, 21, 12, 200, 32)
+        self.bed_motor = StepperMotor(20, 21, 16, 200, 32)
+
+        self.camera_motor = StepperMotor(26,13,19,200,32)
 
 
     def turn_bed(self, degrees):
@@ -58,6 +59,9 @@ class Scanner :
 
 scanner = Scanner()
 
-scanner.turn_bed(360)
-scanner.turn_bed(-360)
+for i in range(36):
+    scanner.turn_bed(10)
+    scanner.camera_motor.turn(10)
+    sleep(0.5)
 
+GPIO.cleanup()
