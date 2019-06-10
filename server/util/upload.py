a6aa0b9a16f80d9670cb3a8e8b6352b5b5a46216
Midouni Billel <midounibillel@gmail.com>
2019-06-10 16:03:31 +0200

    /server: email send without wetransfer

--- server/util/upload.py
+++ server/util/upload.py
@@ -1,14 +1,18 @@
 import sys
 import yagmail
 from . import context_finder as context
+from py3wetransfer import Py3WeTransfer
 
 def send_email_zip_project(project_name):
+    #file_transfer = Py3WeTransfer("73964bc4-939e-4fcd-bf47-31931fd7cf45")
+    file_path = '{}/{}/{}.zip'.format(context.__PROJECTS_PATH__, project_name, project_name)
+    #url_transfer = file_transfer.upload_file(file_path, "upload")
     receiver = "lelbi36@gmail.com"
-    body = "Hello my friend"
-
+    body = "envoie test : "
     yag =  yagmail.SMTP(user="wcscannerter@gmail.com", password="wcscanner06")
     yag.send(
         to=receiver,
         subject = "test send mail",
         contents = body,
+        attachments= file_path
     )
